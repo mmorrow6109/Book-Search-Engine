a5669cd21e883730f957e78c40491dc6428b73fa
mmorrow6109 <msmorrow6109@gmail.com>
2024-04-04 15:07:36 -0700

    queries.js and mutations.js added to utils in clientside

---
+++ Develop/Client/utils/mutations.js
@@ -0,0 +1,69 @@
+import { gql } from '@apollo/client';
+
+// Define the LOGIN_USER mutation
+export const LOGIN_USER = gql`
+  mutation LoginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+// Define the ADD_USER mutation
+export const ADD_USER = gql`
+  mutation AddUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+// Define the SAVE_BOOK mutation
+export const SAVE_BOOK = gql`
+  mutation SaveBook($bookInput: BookInput!) {
+    saveBook(bookInput: $bookInput) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+// Define the REMOVE_BOOK mutation
+export const REMOVE_BOOK = gql`
+  mutation RemoveBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
